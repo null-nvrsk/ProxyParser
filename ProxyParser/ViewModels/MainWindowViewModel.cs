f96a5e78d3b094728e6b00cc9ed22124703c3f65
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-19 06:02:28 +0300

    Add ViewModel for main window

---
+++ ProxyParser/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProxyParser.ViewModels.Base;
+
+namespace ProxyParser.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Window title
+
+        private string _Title = "Default title";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+    }
+}
