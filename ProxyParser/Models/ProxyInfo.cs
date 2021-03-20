ee8adc454970d78b3c2e64ff70f64e6cfb4f6918
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-20 10:28:03 +0300

    Add proxy model

---
+++ ProxyParser/Models/ProxyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyParser.Models
+{
+    public class ProxyInfo
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public DateTime? LastCheck { get; set; } = null;
+        public int? LastPing { get; set; } = null;
+    }
+}
