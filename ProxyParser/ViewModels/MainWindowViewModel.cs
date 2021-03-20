4a75b1870c2e1b261431aedd6bdfc1dad1da335c
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-20 12:13:58 +0300

    Bind commands to main buttons

--- ProxyParser/ViewModels/MainWindowViewModel.cs
+++ ProxyParser/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using ProxyParser.Infrastructure.Commands;
 using ProxyParser.ViewModels.Base;
 
 namespace ProxyParser.ViewModels
@@ -19,6 +22,8 @@ namespace ProxyParser.ViewModels
 
         #endregion
 
+
+        #region Status panel
 
         #region Status - total proxy
 
@@ -79,6 +84,116 @@ namespace ProxyParser.ViewModels
         }
 
         #endregion
+
+        #endregion
+
+        #region Commands
+
+        private bool _parsingStarted = false;
+        private bool _parsingPaused = false;
+
+        #region StartParsingCommand
+
+        public ICommand StartParsingCommand { get; }
+
+        private void OnStartParsingCommandExecuted(object p)
+        {
+            _parsingStarted = true;
+            _parsingPaused = false;
+
+            // Заглушка!!!
+            MessageBox.Show("Parsing started");
+        }
+
+        private bool CanStartParsingCommandExecute(object p) => !_parsingStarted;
+
+        #endregion
+
+        #region PauseParsingCommand
+
+        public ICommand PauseParsingCommand { get; }
+
+        private void OnPauseParsingCommandExecuted(object p)
+        {
+            // Заглушка!!!
+            if (!_parsingPaused)
+            { 
+                _parsingPaused = true;
+                MessageBox.Show("Parsing paused");
+            }
+            else
+            {
+                _parsingPaused = false;
+                MessageBox.Show("Parsing resume");
+            }
+        }
+
+        private bool CanPauseParsingCommandExecute(object p) => _parsingStarted;
+
+        #endregion
+
+        #region StopParsingCommand
+
+        public ICommand StopParsingCommand { get; }
+
+        private void OnStopParsingCommandExecuted(object p)
+        {
+            _parsingStarted = false;
+            _parsingPaused = false;
+
+            // Заглушка!!!
+            MessageBox.Show("Parsing stopped");
+        }
+
+        private bool CanStopParsingCommandExecute(object p) => _parsingStarted;
+
+        #endregion
+
+        #region ClearParsingResultCommand
+
+        public ICommand ClearParsingResultCommand { get; }
+
+        private void OnClearParsingResultCommandExecuted(object p)
+        {
+            // Заглушка!!!
+            MessageBox.Show("Parsing result clear");
+        }
+
+        private bool CanClearParsingResultCommandExecute(object p) => !_parsingStarted;
+
+        #endregion
+
+        #region ExportParsingResultCommand
+
+        public ICommand ExportParsingResultCommand { get; }
+
+        private void OnExportParsingResultCommandExecuted(object p)
+        {
+            // Заглушка!!!
+            MessageBox.Show("Export parsing result");
+        }
+
+        private bool CanExportParsingResultCommandExecute(object p) => !_parsingStarted;
+
+        #endregion
+
+
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            #region Commands
+
+            StartParsingCommand = new RelayCommand(OnStartParsingCommandExecuted, CanStartParsingCommandExecute);
+            PauseParsingCommand = new RelayCommand(OnPauseParsingCommandExecuted, CanPauseParsingCommandExecute);
+            StopParsingCommand = new RelayCommand(OnStopParsingCommandExecuted, CanStopParsingCommandExecute);
+            ClearParsingResultCommand = new RelayCommand(OnClearParsingResultCommandExecuted, CanClearParsingResultCommandExecute);
+            ExportParsingResultCommand = new RelayCommand(OnExportParsingResultCommandExecuted, CanExportParsingResultCommandExecute);
+
+            #endregion
+
+        }
     }
 
 }
