1b6296b2395ed87f81dbaecab060cc5c38b61099
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-20 09:36:01 +0300

    status bar binding

--- ProxyParser/ViewModels/MainWindowViewModel.cs
+++ ProxyParser/ViewModels/MainWindowViewModel.cs
@@ -19,5 +19,66 @@ namespace ProxyParser.ViewModels
 
         #endregion
 
+
+        #region Status - total proxy
+
+        private int _ProxyTotal = 0;
+        /// <summary>Общее кол-во прокси</summary>
+        public int ProxyTotal
+        {
+            get => _ProxyTotal;
+            set => Set(ref _ProxyTotal, value);
+        }
+
+        #endregion
+
+        #region Status - good proxy
+
+        private int _ProxyGood = 0;
+        /// <summary>Кол-во рабочих прокси</summary>
+        public int ProxyGood
+        {
+            get => _ProxyGood;
+            set => Set(ref _ProxyGood, value);
+        }
+
+        #endregion
+
+        #region Status - Bad proxy
+
+        private int _ProxyBad = 0;
+        /// <summary>Кол-во нерабочих прокси</summary>
+        public int ProxyBad
+        {
+            get => _ProxyBad;
+            set => Set(ref _ProxyBad, value);
+        }
+
+        #endregion
+
+        #region Status - Elapsed time
+
+        private TimeSpan _TimeElapsed = TimeSpan.Zero;
+        /// <summary>Прошедшее время</summary>
+        public TimeSpan TimeElapsed
+        {
+            get => _TimeElapsed;
+            set => Set(ref _TimeElapsed, value);
+        }
+
+        #endregion
+
+        #region Status - Estimated time
+
+        private TimeSpan _TimeEstimated = TimeSpan.Zero;
+        /// <summary>Примерное время завершения</summary>
+        public TimeSpan TimeEstimated
+        {
+            get => _TimeEstimated;
+            set => Set(ref _TimeEstimated, value);
+        }
+
+        #endregion
     }
+
 }
