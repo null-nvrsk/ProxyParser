5a1be13026a29ce6eb569ca393d668c9933c9fe2
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-22 07:11:57 +0300

    Test proxy parsing with Selenium

--- ProxyParserConsole/Program.cs
+++ ProxyParserConsole/Program.cs
@@ -1,46 +1,104 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace ProxyParserConsole
 {
     class Program
     {
         // определяет как бота
-        // private const string dataUrl = @"https://hidemy.name/en/proxy-list/?maxtime=1500&type=5&anon=234#list";
+        // Через Selenium работает
+        private const string dataUrl = @"https://hidemy.name/en/proxy-list/?maxtime=1500&type=5&anon=234#list";
         // private const string dataUrl = @"https://spys.one/proxys/US/";
         // private const string dataUrl = @"http://free-proxy.cz/ru/proxylist/country/all/socks5/ping/all";
 
         // требуется cookies и JavaScript
+        // Через Selenium работает
         // private const string dataUrl = @"https://advanced.name/ru/freeproxy?type=socks5";
 
 
         // Через HttpClient работаеет нормально
-        private const string dataUrl = @"https://www.luxuryhomemarketing.com/real-estate-agents/advanced_search.html";
+        // private const string dataUrl = @"https://www.luxuryhomemarketing.com/real-estate-agents/advanced_search.html";
 
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
 
+            // Чистый HTML
             //HttpClient client = new HttpClient();
             //var response = await client.GetAsync(dataUrl);
             //var content = await response.Content.ReadAsStringAsync();
 
             //Console.WriteLine(content);
 
-            var web = new HtmlWeb();
-            var doc = web.Load(dataUrl);
+            // Парсинг через HtmlAgilityPack
+            //var web = new HtmlWeb();
+            //var doc = web.Load(dataUrl);
 
-            var h1 = doc.DocumentNode
-                .SelectNodes("//body//h1")
-                .First().InnerHtml;
+            //var h1 = doc.DocumentNode
+            //    .SelectNodes("//body//h1")
+            //    .First().InnerHtml;
 
-            Console.WriteLine(h1);
+            //Console.WriteLine(h1);
 
-            Console.ReadLine();
+            IWebDriver browser = new ChromeDriver();
+            //Browser.Manage().Window.Maximize();
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Start page
+            browser.Navigate().GoToUrl(dataUrl);
+
+
+            // парсим страницу
+            List<IWebElement> items = browser.FindElements(By.CssSelector("div.table_block tbody tr")).ToList();
+            Console.WriteLine($"Items: {items.Count}");
+
+            int rowCount = 0;
+            foreach (IWebElement row in items)
+            {
+                
+                //Console.WriteLine($"row = {row}");
+
+                var cols = row.FindElements(By.CssSelector("td"));
+                //Console.Write($"({cols.Count}) ");
+
+                if (cols.Count == 7)
+                {
+                    Console.WriteLine($"({++rowCount}) IP = {cols[0].Text}");
+                    Console.WriteLine($"Port = {cols[1].Text}");
+                    try
+                    {
+                        string country = cols[2].FindElement(By.CssSelector("span.country")).Text;
+                        Console.WriteLine($"Country = {country}");
+                    }
+                    catch (Exception e) { throw; }
+
+                    try
+                    {
+                        string city = cols[2].FindElement(By.CssSelector("span.city")).Text;
+                        Console.WriteLine($"City = {city}");
+                    }
+                    catch (Exception e) { throw; }
+
+                    //Console.WriteLine($"Country, City = {cols[2].Text}");
+                    Console.WriteLine($"Speed = {cols[3].Text}");
+                    Console.WriteLine($"Type = {cols[4].Text}");
+                }
+
+                //foreach (var col in cols)
+                //    Console.Write($"[{col.Text}]");
+
+                Console.WriteLine();
+            }
+
+            browser.Quit();
+            //Console.ReadLine();
         }
 
     }
