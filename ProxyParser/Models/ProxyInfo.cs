2710f12ec3a7cf3cb075ef061c5a4c4b2bc9bc0e
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-03-23 07:11:56 +0300

    Add property for proxy model

--- ProxyParser/Models/ProxyInfo.cs
+++ ProxyParser/Models/ProxyInfo.cs
@@ -12,6 +12,7 @@ namespace ProxyParser.Models
         public string State { get; set; }
         public string City { get; set; }
         public DateTime? LastCheck { get; set; } = null;
+        public int? LastSpeed { get; set; } = null;
         public int? LastPing { get; set; } = null;
     }
 }
