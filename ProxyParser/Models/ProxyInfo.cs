fddf2b875467df65f4560ef6f2c800d42b030c20
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-04-08 21:20:53 +0300

    Add proxy type to model

--- ProxyParser/Models/ProxyInfo.cs
+++ ProxyParser/Models/ProxyInfo.cs
@@ -9,11 +9,20 @@ namespace ProxyParser.Models
         public int Id { get; set; }
         public string Ip { get; set; }
         public int Port { get; set; }
+        public ProxyType Type { get; set; }
         public string Country { get; set; }
         public string State { get; set; }
         public string City { get; set; }
         public DateTime? LastCheck { get; set; } = null;
         public int? LastSpeed { get; set; } = null;
         public int? LastPing { get; set; } = null;
+    }
+
+    public class ProxyType
+    {
+        public bool HTTP { get; set; } = false;
+        public bool HTTPS { get; set; } = false;
+        public bool SOCKS4 { get; set; } = false;
+        public bool SOCKS5 { get; set; } = false;
     }
 }
