c08256eecc6ac2fa3a2f647a2168527543fe4400
Maksim Skomorokhov <nullnvrsk@gmail.com>
2021-04-09 06:40:59 +0300

    Move commands to extra class

--- ProxyParser/ViewModels/MainWindowViewModel.cs
+++ ProxyParser/ViewModels/MainWindowViewModel.cs
@@ -14,7 +14,7 @@ using ProxyParser.ViewModels.Base;
 
 namespace ProxyParser.ViewModels
 {
-    internal class MainWindowViewModel : ViewModelBase
+    internal partial class MainWindowViewModel : ViewModelBase
     {
         private ObservableCollection<ProxyInfo> _proxyList = new ObservableCollection<ProxyInfo>();
         /// <summary>Список прокси</summary>
@@ -92,106 +92,30 @@ namespace ProxyParser.ViewModels
 
         #endregion
 
-        #region Commands
+        #region Status - ParsingStarted
 
-        private bool _parsingStarted;
-        private bool _parsingPaused;
+        private bool _parsingStarted = false;
 
-        #region StartParsingCommand
-
-        public ICommand StartParsingCommand { get; }
-
-        private void OnStartParsingCommandExecuted(object p)
+        public bool ParsingStarted
         {
-            _parsingStarted = true;
-            _parsingPaused = false;
-
-            _ = ParseSiteHideMyNameAsync(/*ProxyList*/);
+            get => _parsingStarted;
+            set => Set(ref _parsingStarted, value);
         }
 
-        private bool CanStartParsingCommandExecute(object p) => !_parsingStarted;
-
         #endregion
 
-        #region PauseParsingCommand
+        #region Status - ParsingPaused
 
-        public ICommand PauseParsingCommand { get; }
+        private bool _parsingPaused = false;
 
-        private void OnPauseParsingCommandExecuted(object p)
+        public bool ParsingPaused
         {
-            // Заглушка!!!
-            if (!_parsingPaused)
-            { 
-                _parsingPaused = true;
-                MessageBox.Show("Parsing paused");
-            }
-            else
-            {
-                _parsingPaused = false;
-                MessageBox.Show("Parsing resume");
-            }
+            get => _parsingPaused;
+            set => Set(ref _parsingPaused, value);
         }
 
-        private bool CanPauseParsingCommandExecute(object p) => _parsingStarted;
-
         #endregion
 
-        #region StopParsingCommand
-
-        public ICommand StopParsingCommand { get; }
-
-        private void OnStopParsingCommandExecuted(object p)
-        {
-            _parsingStarted = false;
-            _parsingPaused = false;
-
-            // Заглушка!!!
-            MessageBox.Show("Parsing stopped");
-        }
-
-        private bool CanStopParsingCommandExecute(object p) => _parsingStarted;
-
-        #endregion
-
-        #region ClearParsingResultCommand
-
-        public ICommand ClearParsingResultCommand { get; }
-
-        private void OnClearParsingResultCommandExecuted(object p)
-        {
-            ProxyList.Clear();
-            ProxyTotal = ProxyList.Count;
-        }
-
-        private bool CanClearParsingResultCommandExecute(object p) => (ProxyList.Count > 0);
-
-        #endregion
-
-        #region ExportParsingResultCommand
-
-        public ICommand ExportParsingResultCommand { get; }
-
-        private void OnExportParsingResultCommandExecuted(object p)
-        {
-            try
-            {
-                if (_dialogService.SaveFileDialog() != true) return;
-
-                _fileService.Save(_dialogService.FilePath, ProxyList, _dialogService.ExportType);
-                _dialogService.ShowMessage("Файл сохранен");
-            }
-            catch (Exception ex)
-            {
-                _dialogService.ShowMessage(ex.Message);
-            }
-        }
-
-        private bool CanExportParsingResultCommandExecute(object p) => (ProxyList.Count > 0);
-
-        #endregion
-
-
-        #endregion
 
 
         public MainWindowViewModel()
---
+++ ProxyParser/ViewModels/MainWindowViewModelCommands.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ProxyParser.ViewModels
+{
+    internal partial class MainWindowViewModel
+    {
+
+        #region StartParsingCommand
+
+        public ICommand StartParsingCommand { get; }
+
+        private void OnStartParsingCommandExecuted(object p)
+        {
+            _parsingStarted = true;
+            _parsingPaused = false;
+
+            _ = ParseSiteHideMyNameAsync( /*ProxyList*/);
+        }
+
+        private bool CanStartParsingCommandExecute(object p) => !_parsingStarted;
+
+        #endregion
+
+        #region PauseParsingCommand
+
+        public ICommand PauseParsingCommand { get; }
+
+        private void OnPauseParsingCommandExecuted(object p)
+        {
+            // Заглушка!!!
+            if (!_parsingPaused)
+            {
+                _parsingPaused = true;
+                MessageBox.Show("Parsing paused");
+            }
+            else
+            {
+                _parsingPaused = false;
+                MessageBox.Show("Parsing resume");
+            }
+        }
+
+        private bool CanPauseParsingCommandExecute(object p) => _parsingStarted;
+
+        #endregion
+
+        #region StopParsingCommand
+
+        public ICommand StopParsingCommand { get; }
+
+        private void OnStopParsingCommandExecuted(object p)
+        {
+            _parsingStarted = false;
+            _parsingPaused = false;
+
+            // Заглушка!!!
+            MessageBox.Show("Parsing stopped");
+        }
+
+        private bool CanStopParsingCommandExecute(object p) => _parsingStarted;
+
+        #endregion
+
+        #region ClearParsingResultCommand
+
+        public ICommand ClearParsingResultCommand { get; }
+
+        private void OnClearParsingResultCommandExecuted(object p)
+        {
+            ProxyList.Clear();
+            ProxyTotal = ProxyList.Count;
+        }
+
+        private bool CanClearParsingResultCommandExecute(object p) => (ProxyList.Count > 0);
+
+        #endregion
+
+        #region ExportParsingResultCommand
+
+        public ICommand ExportParsingResultCommand { get; }
+
+        private void OnExportParsingResultCommandExecuted(object p)
+        {
+            try
+            {
+                if (_dialogService.SaveFileDialog() != true) return;
+
+                _fileService.Save(_dialogService.FilePath, ProxyList, _dialogService.ExportType);
+                _dialogService.ShowMessage("Файл сохранен");
+            }
+            catch (Exception ex)
+            {
+                _dialogService.ShowMessage(ex.Message);
+            }
+        }
+
+        private bool CanExportParsingResultCommandExecute(object p) => (ProxyList.Count > 0);
+
+        #endregion
+    }
+}
